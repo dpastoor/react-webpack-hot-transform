4dcb59004f98d4b6e9b5e5285bbb830e2b3b77ca
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-17 16:29:26 -0800

    update deps and provide more indepth initial starter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './css/styles.css'
+import Counter from './Components/Counter.js';
+import _ from 'lodash';
+import './css/styles.css';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      counters: [
+        {
+          name: "counter 1",
+          value: 0
+        },
+        {
+          name: "counter 2",
+          value: 0
+        }
+      ]
+    };
   }
+
+  incrementCounter(index) {
+    let counters = this.state.counters;
+    counters[index].value = counters[index].value + 1;
+    this.setState({
+      counters: counters
+    });
+  }
+
   render() {
+    let counters = _.map(this.state.counters, (counter, index) => {
+      return(
+      <div
+        onClick={() => this.incrementCounter(index)}
+        >
+        <Counter value={counter.value}></Counter>
+      </div>
+      )
+    });
+
     return (
        <div className="App">
-       <h1>hello from {this.props.name}</h1>
+         Total Counter Value: {_.sum(_.map(this.state.counters, "value"))}
+         {counters}
        </div>
     );
   }
 }
-
-
-
